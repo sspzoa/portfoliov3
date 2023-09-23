23c00632161d5fb8c3e5cb962ccb9d6376f1a5f5
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-09-24 00:22:33 +0900

    edit projects

--- src/components/Home/Projects/Projects.tsx
+++ src/components/Home/Projects/Projects.tsx
@@ -5,25 +5,30 @@ export default function Projects() {
     return (
         <div id='projects' className={styles.container}>
             <h1>My Projects</h1>
-            {projectData.map((project, index) => (
-                <div key={index} className={styles.box}>
-                    <img id={styles[project.image.split('/')[2].split('.')[0]]} src={project.image} />
-                    <div className={styles.textBox}>
-                        <div className={styles.row}>
-                            <a href={project.obsidian} target="_blank" rel="noopener noreferrer">
-                                <h1>{project.title}</h1></a><p>{project.teamSize}</p>
+            <div className={styles.projects}>
+                {projectData.map((project, index) => (
+                    <div key={index} className={styles.projects}>
+                        <div className={styles.project}>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer"><img src={project.image} alt={project.title}/></a>
+                            <div className={styles.textBox}>
+                                <div className={styles.title}>
+                                    <h1>{project.title}</h1>
+                                    <p>{project.teamSize}</p>
+                                </div>
+                                <p>{project.description}</p>
+                                <p>{project.period} | <a href={project.github} target="_blank" rel="noopener noreferrer" id={styles.link}>GitHub</a></p>
+                                <div className={styles.tags}>
+                                    {project.tag.split(', ').map((tag, tagIndex) => (
+                                        <div key={tagIndex} className={styles.tag}>
+                                            {tag}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
                         </div>
-                        {project.description.map((desc, descIndex) => (
-                            <p key={descIndex}>{desc}</p>
-                        ))}
-                        <hr className={styles.horizontalRule} />
-                        <p>주요기능 : <br />{project.keyFeature}</p>
-                        <p>Github: <a href={project.githubLink} target="_blank" rel="noopener noreferrer">{project.github}</a></p>
-                        <p>Demo: <a href={project.demoLink} target="_blank" rel="noopener noreferrer">{project.demo}</a></p>
-                        <p>App: {project.app}</p>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
