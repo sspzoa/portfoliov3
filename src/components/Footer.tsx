c1833183c71e2951643e341b15b25cdc0b19dfe5
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-09-24 14:32:39 +0900

    edit ui

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,15 @@ import {faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons';
 export default function Footer() {
     return (
         <div className={styles.footer}>
-            <a href="https://github.com/sspzoa" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faGithub} className={styles.icon} />
-            </a>
-            <a href="https://www.instagram.com/_s._.pyo/" target="_blank" rel="noopener noreferrer">
-                <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
-            </a>
+            <div className={styles.icons}>
+                <a href="https://github.com/sspzoa" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+                </a>
+                <a href="https://www.instagram.com/_s._.pyo/" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+                </a>
+            </div>
             <h1>&copy; 2023 Seungpyo Suh. All rights reserved.</h1>
-            <h1>¯\_(ツ)_/¯</h1>
         </div>
     )
 }
