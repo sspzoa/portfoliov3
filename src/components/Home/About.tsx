38ac9dc9fa991e9da174b3ecdba9b36f416c9db6
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-09-25 22:14:59 +0900

    about

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -8,20 +8,23 @@ export default function About() {
                 <img src='/About/profile.jpeg'/>
                 <div className={styles.textBox}>
                     <p>
-                        안녕하세요! 저는 현재 <span className={styles.kdmhs}>한국디지털미디어고등학교</span>에 재학 중이며 앱 개발을 공부하고 있는 고등학생 개발자입니다.
+                        안녕하세요! 저는 현재 <span className={styles.kdmhs}>한국디지털미디어고등학교</span>에 재학 중이며 개발을 공부하고 있는 고등학생 개발자입니다.
                     </p>
                     <p>
                         이전에 <span className={styles.views}>Views</span> 방식의 안드로이드 앱 개발을 공부하였고, 현재는 <span
                         className={styles.jetpackcompose}>Jetpack Compose</span>를 이용한 안드로이드 앱 개발과 <span
-                        className={styles.flutter}>Flutter</span>와 <span
-                        className={styles.reactnative}>React Native</span>를 이용한 크로스 플랫폼 개발에 집중하고 있습니다.
+                        className={styles.flutter}>크로스 플랫폼</span> 개발에 집중하고 있습니다.
                     </p>
                     <p>
-                        앱 이외에도 <span className={styles.next}>웹 프론트</span>, <span
-                        className={styles.node}>서버 개발</span> 등을 경험해 보았습니다.
+                        또 저는 <span
+                        className={styles.web3}>Web3와 블록체인</span>에 굉장한 흥미를 가지고 있습니다. 단순히 <span
+                        className={styles.bitcoin}>가상화폐</span>의 개념을 넘어 이러한 기술의 발전이 각각의 측면에서 어떠한 파급 효과를 가져올지에 대해 큰
+                        기대감을 가지고 있습니다.
                     </p>
                     <p>
-                        기술의 다양한 분야에서 경험을 쌓으며, 끊임없이 발전하는 개발자가 되기 위해 노력하고 있습니다!
+                        기술의 <span
+                        className={styles.manymany}>다양한 분야</span>에서 경험을 쌓으며, <span
+                        className={styles.study}>끊임없이 발전하는 개발자</span>가 되기 위해 노력하고 있습니다!
                     </p>
                 </div>
             </div>
